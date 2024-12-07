427c89a0a6f7c62ad51221c67151a5dcf8fa2a22
aradsab <ediarad@yahoo.co.uk>
2024-12-06 16:03:13 -0800

    not done yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import {NavigationBar} from "./component/NavigationBar";
 import { Banner } from './component/Banner';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Skills } from './component/Skills';
 
 function App() {
   return (
       <div className="App">
         <NavigationBar />
         <Banner />
+        <Skills />
       </div>
   );
 }
