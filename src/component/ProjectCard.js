fe3030d46ae5f24a16d35096d7fbc673b3f7baaa
aradprsabet <aradpr@students.cs.ubc.ca>
2024-12-16 17:52:28 -0800

    Update ProjectCard.js

--- src/component/ProjectCard.js
+++ src/component/ProjectCard.js
@@ -8,7 +8,7 @@ export const ProjectCard = ({ title, description, imgUrl, githubLink }) => {
     return (
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx" onClick={handleClick} style={{ cursor: "pointer" }}>
-                <img src={imgUrl} alt={`${title} preview`} />
+                <img className="img-fluid" src={imgUrl} alt={`${title} preview`} />
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
